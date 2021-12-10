fe305f00991052a8940b233b5800cb51c0469688
Kruti Akbari <aakkbbaarriikruti1010@gmail.com>
2021-12-10 17:52:03 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BookService } from './book.service';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookAddComponent } from './book-add/book-add.component';
+import { BookGetComponent } from './book-get/book-get.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { SearchFilterPipe } from './search-filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookAddComponent,
+    BookGetComponent,
+    BookEditComponent,
+    SearchFilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [BookService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
